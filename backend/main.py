972d7d94f4994d4b9799b8593e2ff4345dc3600c
jfpoilpret <jfpoilpret@gmail.com>
2018-07-27 17:56:14 +0200

    Update to angular5, remove CORS, use susbcription to observable, add simple auth on backend.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from flask_cors import CORS
+from flask_httpauth import HTTPBasicAuth
 from json import dumps
 
 app = Flask(__name__)
-CORS(app)
+auth = HTTPBasicAuth()
+
+users = {
+    'jfp': 'jfp'
+}
 
 todos = [
     {'id': 1, 'title': 'Hacer la compra'},
@@ -13,11 +17,16 @@ todos = [
     {'id': 3, 'title': 'Colgar el cuadro del salón'},
 ]
 
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
 
 @app.route('/todos', methods=['GET'])
+@auth.login_required
 def get_todos():
     return dumps(todos), 200
-
 
 if __name__ == '__main__':
     app.run(
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 import { Todo } from './todo';
 import { TodosService } from './todos.service';
@@ -13,10 +13,14 @@ import { TodosService } from './todos.service';
 export class AppComponent implements OnInit {
 
   todos$: Observable<Todo[]>;
+  todos2: Todo[];
 
   constructor(private _todosService: TodosService) { }
 
   ngOnInit(): void {
     this.todos$ = this._todosService.getTodos();
+    this._todosService.getTodos().subscribe(
+      data => this.todos2 = data
+    );
   }
 }
