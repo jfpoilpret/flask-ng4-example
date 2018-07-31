edbae69dded3ecc615eba761779fbddbd6543a83
jfpoilpret <jfpoilpret@gmail.com>
2018-07-31 22:41:53 +0200

    Redo with simple falcon.

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask_httpauth import HTTPBasicAuth
-from json import dumps
-
-app = Flask(__name__)
-auth = HTTPBasicAuth()
+import json
+import falcon
 
 users = {
     'jfp': 'jfp'
@@ -17,19 +13,15 @@ todos = [
     {'id': 3, 'title': 'Colgar el cuadro del salón'},
 ]
 
-@auth.get_password
 def get_pw(username):
     if username in users:
         return users.get(username)
     return None
 
-@app.route('/todos', methods=['GET'])
-@auth.login_required
-def get_todos():
-    return dumps(todos), 200
+class Todo(object):
+    def on_get(self, req, resp):
+        resp.body = json.dumps(todos, ensure_ascii =  False)
 
-if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000
-    )
+app = falcon.API()
+app.add_route('/todos', Todo())
+
